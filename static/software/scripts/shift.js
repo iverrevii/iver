623c6011f474ab514ef7e54bbad1a37c77c3a2c1
Ed Botor <edbotor@Eds-MacBook-Pro.local>
2019-01-13 21:02:22 +1000

    EDIT

--- static/software/scripts/shift.js
+++ static/software/scripts/shift.js
@@ -13,6 +13,6 @@ $(window).resize(titleShift);
 
 function titleShift() {
     if($(window).width() < 500) {
-        $(".homeDiv .subTitle").html("JAVA | C | ARDUINO | ASSEMBLY | WEB");
+        $(".homeDiv .subTitle").html("BUSINESS PROJECTS| BUILDING PLANS | INVESTMENTS");
     }
 }
